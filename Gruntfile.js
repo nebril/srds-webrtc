57e144a6bf5d93872ad9ff23174925f415a64d17
Maciej Kwiek <maciej.iai@gmail.com>
2014-06-16 19:17:05 +0200

    Bower problems. Install bower_components to both webrtc and webrtc/app

--- Gruntfile.js
+++ Gruntfile.js
@@ -419,7 +419,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('build', [
     'clean:dist',
-    'wiredep',
+    //'wiredep',
     'useminPrepare',
     'concurrent:dist',
     'autoprefixer',
